cffa4652110ecc973a6f6e7f671f9f1d047192f0
TUNÇ Selman <selmantunc@yandex.com>
2024-10-01 21:06:05 -0600

    Update index.php bootsrap-grid.css
    bootsrap-grid.css added

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 function stnc_engine_admin_enqueue_style()
 {   
   wp_enqueue_style('stncstyle', plugins_url('assets/css/ss-system-custom-post-admin.css', __FILE__),"","1.4.28");
-  wp_enqueue_style('stncpico', plugins_url('assets/css/pico.min.css', __FILE__),"","1.7");
+  wp_enqueue_style('stncpico', plugins_url('assets/css/bootsrap-grid.css', __FILE__),"","1.7");
 }
 
 add_action('admin_enqueue_scripts', 'stnc_engine_admin_enqueue_style');
